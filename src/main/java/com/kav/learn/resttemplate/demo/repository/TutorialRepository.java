feec952e33ff76ac2f69fa85399de19597b22369
kavithasimac <kavithasubbiah@hotmail.com>
2021-09-12 11:45:56 -0400

    working cassandra repository

--- src/main/java/com/kav/learn/resttemplate/demo/repository/TutorialRepository.java
+++ src/main/java/com/kav/learn/resttemplate/demo/repository/TutorialRepository.java
@@ -1,9 +1,14 @@
 package com.kav.learn.resttemplate.demo.repository;
 
 import com.kav.learn.resttemplate.demo.entities.Tutorial;
+import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TutorialRepository extends CassandraRepository<Tutorial, UUID> {
+
+    @AllowFiltering
+    List<Tutorial> findByPublished(boolean published);
 }
